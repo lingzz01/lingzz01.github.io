a8875175e11b9b82eebea3cd0105503866cdf0a2
lingzizheng <lzheng@risd.edu>
2018-08-16 14:10:17 -0400

    website

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+//
+//var myString = "You used to tell me how you didn't like the way I lacked a sense of intimacy";
+//var myArray = myString.split("");
+//var loopTimer;
+//function frameLooper() {
+//	if(myArray.length > 0) {
+//		document.getElementById("one").innerHTML += myArray.shift();
+//	} else {
+//		clearTimeout(loopTimer); 
+//                return false;
+//	}
+//	loopTimer = setTimeout(frameLooper(),70);
+//}
+//
+//frameLooper();
+
+
+//
+//function myMove() {
+//var elem = document.getElementById("one");
+//var elema = document.getElementById("two");
+//var pos = 0;
+//var id = setInterval(frame, 10);
+//
+//  function frame() {
+//    if (pos == 100) {
+//      clearInterval(id);
+//    } else {
+//      pos++; 
+//      elem.style.left = pos + 'px';   
+//    }
+//  } 
+//var id2 = setInterval(frame2,200);   
+//    
+//     function frame2() {
+//    if (pos == 100) {
+//      clearInterval(id);
+//    } else {
+//      pos++; 
+//      elema.style.left = pos + 'px';   
+//    }
+//  }
+//       
+//    
+//    
+//}
---
+++ sth.js
@@ -0,0 +1,12 @@
+function newVideo() {
+
+    document.getElementById("video-bg").src="02.mp4";    
+
+}
+
+
+function oldVideo() {
+
+    document.getElementById("video-bg").src="If we were together.mp4";    
+
+}
